ec6da9bb7c4e0db366384f124444e247fb0e5b9a
Marijan Brvar <marijan.brvar@gmail.com>
2021-08-30 08:50:40 +0200

    setting up react and create and add header to layout

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import './Header.css';
+
+const Header = () => (
+  <header>
+    <div className="menu">
+      <div>Book store</div>
+      <ul>
+        <li>Books</li>
+        <li>Categories</li>
+      </ul>
+    </div>
+    <div>
+      User
+    </div>
+  </header>
+);
+
+export default Header;
