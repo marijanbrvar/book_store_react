325a42570b4a8062fecf96cf98bd530188300c91
Marijan Brvar <marijan.brvar@gmail.com>
2021-09-02 17:00:39 +0200

    Add store add record and show in entry file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import store from './redux/store';
+
+store.dispatch({
+  type: 'addBook',
+  payload: {
+    title: 'LOTR The Fellowship of the Ring',
+    category: 'Fantasy',
+    author: 'J.R.R. Tolkien',
+    pages: 606,
+    completed: 35,
+    chapter: 4,
+  },
+});
+
+console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
