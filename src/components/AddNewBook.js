46f819bda394c3a95c67f67f762fec242dd9ff97
Marijan Brvar <marijan.brvar@gmail.com>
2021-09-03 07:11:45 +0200

    Refactore add new book on submit method

--- src/components/AddNewBook.js
+++ src/components/AddNewBook.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books';
 import './addNewBook.css';
 
 const AddNewBook = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
   const dispatch = useDispatch();
   const options = ['Fantasy', 'Philosophy', 'ScienceFiction'];
   const category = options.map((o) => (
     <option key={o}>{o}</option>
   ));
+
+  const addNeBook = (e) => {
+    const form = document.querySelector('FORM');
+    e.preventDefault();
+    const book = {
+      title,
+      author,
+      category: form.category.value,
+      pages: 100,
+      completed: 0,
+      chapter: 0,
+    };
+
+    dispatch(addBook(book));
+  };
 
   return (
     <div className="new-book">
@@ -17,28 +34,15 @@ const AddNewBook = () => {
       </div>
       <div>
         <form
-          onSubmit={(e) => {
-            const form = document.querySelector('FORM');
-            e.preventDefault();
-            const book = {
-              title: form.title.value,
-              author: form.author.value,
-              category: form.category.value,
-              pages: 100,
-              completed: 0,
-              chapter: 0,
-            };
-
-            dispatch(addBook(book));
-          }}
+          onSubmit={addNeBook}
         >
-          <input type="text" name="title" placeholder="Title" required />
-          <input type="text" name="author" placeholder="Author" required />
+          <input type="text" name="title" placeholder="Title" required onChange={(e) => setTitle(e.target.value)} />
+          <input type="text" name="author" placeholder="Author" required onChange={(e) => setAuthor(e.target.value)} />
           <select name="category" required>
             {category}
           </select>
 
-          <button type="submit" className="submitBtn">Add</button>
+          <button type="submit" className="submit-btn">Add</button>
         </form>
       </div>
     </div>
